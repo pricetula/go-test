60f54d26ca9f0e8ad1d7372fb49dafac25b35780
price tula <pricetulak@gmail.com>
2018-07-29 19:48:24 +0300

    pointers

--- pointer/main.go
+++ pointer/main.go
@@ -6,20 +6,23 @@ import (
 
 // Pointer is a variable that contains memory address which points to a value of specific type
 // var identifier *pointer-type = [memory address]
-// &identifierVariable = [memory address]
+// &identifierVariable => [get memory address]
 func main() {
 	var a *int = &testA
 
 	// variable a has memory address which points to variable testA contents
 	fmt.Println(a)
 
-	testA = 90
-
 	// variable b is now a pointer since variable a's contents have been copied to b
 	// any change to testA variable will also affect that pointed with a and b since they both point to same memory location
 	b := a
 
 	fmt.Println(*a, *b, &b) // NB &b is the address of pointer which contains memory address of variable testA
+
+	testA = 239
+
+	// will print 239 since the value pointed by a and b (pointers) has changed to 239
+	fmt.Println(*a, *b, &b)
 }
 
 // This variable is stored in heap
